0bd23c50c8b07e58ba86ca709d62e4324af5af85
SwiderskJ <janswiderski@op.pl>
2023-03-08 15:39:34 +0100

    Update views.py

--- rooms_app/views.py
+++ rooms_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rooms_app.models import Room
+from rooms_app.forms import RoomForm
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class DemoView(View):
+
+    def get(self, request):
+        all_room_list = Room.objects.all().order_by('name')
+        return render(request, 'demo_view.html', context={'rooms': all_room_list})
+
