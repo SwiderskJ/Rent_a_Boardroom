21334e89135e0e1bd40b1684f378d51db6302839
SwiderskJ <janswiderski@op.pl>
2023-03-08 15:03:04 +0100

    Update models.py

--- rooms_app/models.py
+++ rooms_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Room(models.Model):
     name = models.CharField(max_length=24)
+    description = models.CharField(max_length=256)
     city = models.CharField(max_length=60)
     street = models.CharField(max_length=60)
     street_number = models.CharField(max_length=12)
