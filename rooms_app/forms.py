fdcaa4a25e801be37bfc4d6d285fadf529475d23
SwiderskJ <janswiderski@op.pl>
2023-03-08 15:13:33 +0100

    Create forms.py

---
+++ rooms_app/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from models import Room
+
+
+class RoomForm(forms.Form):
+    name = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    city = forms.CharField()
+    street = forms.CharField()
+    street_number = forms.CharField()
+    local_number = forms.IntegerField()
+    post_code_first = forms.IntegerField()
+    post_code_second = forms.IntegerField()
+    level = forms.IntegerField()
+    capacity = forms.IntegerField()
+    projector = forms.BooleanField()
+    seats = forms.IntegerField()
+    tables = forms.BooleanField()
+    catering = forms.BooleanField()
+    private_parking = forms.BooleanField()
+    sound_system = forms.BooleanField()
