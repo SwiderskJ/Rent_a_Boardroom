40d2ba5eb222e9e947f21a82bb95b509e229014a
SwiderskJ <janswiderski@op.pl>
2023-03-10 12:52:07 +0100

    Update urls.py

--- Rent_a_boardroom/urls.py
+++ Rent_a_boardroom/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rooms_app.views import DemoView, ListRoomsView, RoomDetailsView, AddRoomView, AvaiableRoomView, ReservationView
+
+from reservation_app.views import ReservationView
+from rooms_app.views import DemoView, ListRoomsView, RoomDetailsView, AddRoomView, AvaiableRoomView, EditRoomView, \
+    DeleteRoomView, ManageRoomsView
 from user_app.views import UserCreateView, LoginView, LogoutView
 
 urlpatterns = [
@@ -23,11 +26,13 @@ urlpatterns = [
     path('', DemoView.as_view(), name='demo_page'),
     path('room_list/', ListRoomsView.as_view(), name='room_list'),
     path('room_details/<int:room_id>/', RoomDetailsView.as_view(), name='room_detail'),
+    path('manage/', ManageRoomsView.as_view(), name='rooms_management'),
     path('add_room/', AddRoomView.as_view(), name="add_room"),
+    path('edit_room/<int:room_id>/', EditRoomView.as_view(), name='edit_room'),
+    path('delate_room/<int:room_id>/', DeleteRoomView.as_view(), name='delete_room'),
     path('rooms_to_rent/', AvaiableRoomView.as_view(), name="rooms_to_rent"),
     path('registration/', UserCreateView.as_view(), name="register"),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('reservations/', ReservationView.as_view(), name='user_reservation')
-
 ]
